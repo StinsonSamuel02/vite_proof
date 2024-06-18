6cbc7f3596796332c765e360df85646e0f90bf0f
Jose Luis Escobar Gamez <userjleg@gmail.com>
2024-06-17 20:37:30 -0400

    fix: Some organization

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/static/",
+  base: "/static/client/",
   build: {
     manifest: true,
     outDir: resolve("./dist"),
